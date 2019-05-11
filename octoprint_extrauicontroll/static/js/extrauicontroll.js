011f4f6b82d1f2cfd3fb8e163ca76beecc7fd07d
adr76 <oleksander.druz@gmail.com>
2019-05-11 12:56:56 +0300

    Fan Speed controll

--- octoprint_extrauicontroll/static/js/extrauicontroll.js
+++ octoprint_extrauicontroll/static/js/extrauicontroll.js
@@ -11,14 +11,10 @@ $(function() {
         
         //---- Fan Speed controll
         self.control.fanSpeed = new ko.observable(60);      
-
-        self.control.fanSpeedToPwm = ko.pureComputed(function () {
-			self.speed = self.control.fanSpeed() * 255 / 100;
-			return Math.round(self.speed);
-		});        
-
+        
 		self.control.sendFanSpeed = function () {
-			self.control.sendCustomCommand({ command: "M106 S" + self.control.fanSpeedToPwm() });
+            self.speed = Math.round(self.control.fanSpeed() * 255 / 100);
+			self.control.sendCustomCommand({ command: "M106 S" + self.speed });
 		};
         
         // Atach id to elements 
@@ -29,10 +25,11 @@ $(function() {
             // Remove original fan on/off buttons
             $("#fan-on").remove();
             $("#fan-off").remove();
-            // Add new fan controls
+            // Add Fan Speed controll
             $("#control-jog-general").find("button").eq(0).before("\
+                <!-- Fan Speed controll -->\
                 <input type=\"number\" style=\"width: 95px\" data-bind=\"slider: {min: 00, max: 100, step: 1, value: fanSpeed, tooltip: 'hide'}\">\
-                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser() && !islocked(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ":<span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
+                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ":<span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
                 </div>\
            ");
         }
