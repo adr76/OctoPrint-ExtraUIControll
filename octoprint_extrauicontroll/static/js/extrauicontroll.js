83d8c9d3fe29e1467142cd18056459a38a2f6900
adr76 <oleksander.druz@gmail.com>
2019-05-11 14:59:11 +0300

    #control-jog-general filament change controlls

--- octoprint_extrauicontroll/static/js/extrauicontroll.js
+++ octoprint_extrauicontroll/static/js/extrauicontroll.js
@@ -7,24 +7,71 @@ $(function() {
         var self = this;
         
         self.control = parameters[0];        
+ 
+        self.control.filamentChange = function () {
+			self.control.sendCustomCommand({ command: "\
+            M104 S230 ; Set Extr Temp
+            M109 S230 ; Waite Ext Temp
+            G91   ; Relative XYZ
+            G1 Z+10 E-3 F1500
+            G1 E-150 F3000
+            G1 E-150 F3000
+            G1 E-150 F3000
+            G1 E-150 F3000
+            G1 E-100 F3000
+            G1 E-100 F1200
+            G90\
+            "});
+		};
         
+        self.control.filamentLoad = function () {
+			self.control.sendCustomCommand({ command: "\
+            M104 S230 ; Set Extr Temp
+            M109 S230 ; Waite Ext Temp
+            G91   ; Relative XYZ
+            G1 E150 F3000
+            G1 E150 F3000
+            G1 E150 F3000
+            G1 E150 F3000
+            G1 E100 F3000
+            G1 E50 F1500
+            G1 E-1 F1200
+            G90\
+            "});
+		};
+        
+        // #control-jog-general
+        // --------------------
+        $("#control-jog-extrusion").find("button").eq(0).attr("id", "tool");
+        $("#control-jog-extrusion").find("button").eq(1).attr("id", "extrude");
+        $("#control-jog-extrusion").find("button").eq(2).attr("id", "retract");
+        $("#control-jog-extrusion").find("button").eq(2).attr("id", "flow");
+        $("#retract").before("\
+            <div class=\"btn-group\">\
+                <button class=\"btn \" id=\"f-change\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.filamentChange() }\">F-Change</button>\
+                <button class=\"btn \" id=\"f-load\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.filamentLoad() }\">F-Load</button>\
+            </div>
+        ");
+        
+        // #control-jog-general
+        // --------------------
         self.control.light = new ko.observable(30);   
         self.control.fanSpeed = new ko.observable(60);      
         
-		self.control.sendLight = function () {
+		self.control.setLight = function () {
             self.light = Math.round(self.control.light() * 255 / 100);
 			self.control.sendCustomCommand({ command: "M42 P6 S" + self.speed });
 		};
         
-        self.control.sendFanSpeed = function () {
+        self.control.setFanSpeed = function () {
             self.speed = Math.round(self.control.fanSpeed() * 255 / 100);
 			self.control.sendCustomCommand({ command: "M106 S" + self.speed });
 		};
         
-        // Atach id to elements 
         $("#control-jog-general").find("button").eq(0).attr("id", "motors-off");
         $("#control-jog-general").find("button").eq(1).attr("id", "fan-on");
         $("#control-jog-general").find("button").eq(2).attr("id", "fan-off");
+
         if ($("#touch body").length == 0) {
             // Remove original fan on/off buttons
             $("#fan-on").remove();
@@ -33,11 +80,11 @@ $(function() {
             $("#control-jog-general").find("button").eq(0).before("\
             <!-- Light controll -->\
                 <input type=\"number\" style=\"width: 95px\" data-bind=\"slider: {min: 00, max: 100, step: 1, value: light, tooltip: 'hide'}\">\
-                <button class=\"btn btn-block control-box\" id=\"light-set\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendLight() }\">" + gettext("Light") + ": <span data-bind=\"text: light() + '%'\"></span></button>\
+                <button class=\"btn btn-block control-box\" id=\"light-set\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.setLight() }\">" + gettext("Set light") + ": <span data-bind=\"text: light() + '%'\"></span></button>\
                 </div>\
             <!-- Fan Speed controll -->\
                 <input type=\"number\" style=\"width: 95px\" data-bind=\"slider: {min: 00, max: 100, step: 1, value: fanSpeed, tooltip: 'hide'}\">\
-                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ": <span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
+                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.setFanSpeed() }\">" + gettext("Set fan") + ": <span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
                 </div>\
            ");
         }
