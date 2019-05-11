08fe69be39760ccd1e8ef965d7c292d8e39b6a02
adr76 <oleksander.druz@gmail.com>
2019-05-11 13:38:42 +0300

    Add Light controll

--- octoprint_extrauicontroll/static/js/extrauicontroll.js
+++ octoprint_extrauicontroll/static/js/extrauicontroll.js
@@ -8,11 +8,15 @@ $(function() {
         
         self.control = parameters[0];        
         
-        
-        //---- Fan Speed controll
+        self.control.light = new ko.observable(30);   
         self.control.fanSpeed = new ko.observable(60);      
         
-		self.control.sendFanSpeed = function () {
+		self.control.sendLight = function () {
+            self.light = Math.round(self.control.light() * 255 / 100);
+			self.control.sendCustomCommand({ command: "M42 P6 S" + self.speed });
+		};
+        
+        self.control.sendFanSpeed = function () {
             self.speed = Math.round(self.control.fanSpeed() * 255 / 100);
 			self.control.sendCustomCommand({ command: "M106 S" + self.speed });
 		};
@@ -27,9 +31,13 @@ $(function() {
             $("#fan-off").remove();
             // Add Fan Speed controll
             $("#control-jog-general").find("button").eq(0).before("\
-                <!-- Fan Speed controll -->\
+            <!-- Light controll -->\
+                <input type=\"number\" style=\"width: 95px\" data-bind=\"slider: {min: 00, max: 100, step: 1, value: light, tooltip: 'hide'}\">\
+                <button class=\"btn btn-block control-box\" id=\"light-set\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendLight() }\">" + gettext("Light") + ": <span data-bind=\"text: light() + '%'\"></span></button>\
+                </div>\
+            <!-- Fan Speed controll -->\
                 <input type=\"number\" style=\"width: 95px\" data-bind=\"slider: {min: 00, max: 100, step: 1, value: fanSpeed, tooltip: 'hide'}\">\
-                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ":<span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
+                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ": <span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
                 </div>\
            ");
         }
