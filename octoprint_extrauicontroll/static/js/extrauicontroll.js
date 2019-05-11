ae0efd58f69ace082dc0bd72e60a7546bdd4391b
adr76 <oleksander.druz@gmail.com>
2019-05-11 10:54:09 +0300

    Fan Speed controll

--- octoprint_extrauicontroll/static/js/extrauicontroll.js
+++ octoprint_extrauicontroll/static/js/extrauicontroll.js
@@ -6,17 +6,18 @@ $(function() {
     function ExtraUIControllViewModel(parameters) {
         var self = this;
         
-        self.control = parameters[0];
-        self.control.fanSpeed = new ko.observable(60);
+        self.control = parameters[0];        
         
+        
+        // Fan Speed controll
+        self.control.fanSpeed = new ko.observable(60);      
+
         self.control.fanSpeedToPwm = ko.pureComputed(function () {
 			self.speed = self.control.fanSpeed() * 255 / 100;
-			return self.speed;
-		});
-        
-        //send gcode to set fan speed
+			return Math.round(self.speed);
+		});        
+
 		self.control.sendFanSpeed = function () {
-			//self.control.checkSliderValue();
 			self.control.sendCustomCommand({ command: "M106 S" + self.control.fanSpeedToPwm() });
 		};
         
@@ -25,13 +26,16 @@ $(function() {
         $("#control-jog-general").find("button").eq(1).attr("id", "fan-on");
         $("#control-jog-general").find("button").eq(2).attr("id", "fan-off");
         if ($("#touch body").length == 0) {
-            //remove original fan on/off buttons
+            // Remove original fan on/off buttons
             $("#fan-on").remove();
             $("#fan-off").remove();
-            //add new fan controls
+            // Add new fan controls
             $("#control-jog-general").find("button").eq(0).before("\
                 <input type=\"number\" style=\"width: 95px\" data-bind=\"slider: {min: 00, max: 100, step: 1, value: fanSpeed, tooltip: 'hide'}\">\
-                <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ":<span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
+                <div class=\"btn-group\">\
+                    <button class=\"btn btn-block control-box\" id=\"fan-on\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendFanSpeed() }\">" + gettext("Fan speed") + ":<span data-bind=\"text: fanSpeed() + '%'\"></span></button>\
+                    <button class=\"btn \" id=\"fan-off\" data-bind=\"enable: isOperational() && loginState.isUser(), click: function() { $root.sendCustomCommand({ type: 'command', commands: ['M106 S0'] }) }\">" + gettext("off") + "</button>\
+                </div>\
            ");
         }
     }
